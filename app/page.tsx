f91f9f35f7920996545fcef13990f3f8b7fbdf1b
Marcelo <mbonoronha@gmail.com>
2025-07-12 23:46:17 -0300

    inicial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl underline">Welcome to my app!</h1>
+      <Button>Lets get it done!</Button>
+    </div>
+  );
+};
+
+export default Page;
